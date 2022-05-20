14467dbdf76fe19ff9433c5585d8215a3025fe19
Nathan Jaremko <njaremko@wealthsimple.com>
2022-05-19 18:14:56 -0700

    Add resolve remotely feature

---
+++ README.md
@@ -0,0 +1,63 @@
+# First Kind JS/TS SDK
+
+This SDK facilitates using the client APIs provided by [First Kind Software](https://firstkindsoftware.com)
+
+# Usage
+
+```js
+import { fetchFlag } from "@firstkind/sdk";
+
+async function loadPage() {
+  const targetingResult = await fetchFlag({
+    apiKey: "pk_FpACzwidBsbDiaqPloKnCfESozthonuuLbGYSLvb",
+    userId: "u-1234",
+    projectKey: "example-app",
+    environmentKey: "development",
+    flagKey: "show-new-loading-screen",
+    defaultValue: false,
+  });
+
+  if (targetingResult) {
+    return "new loading screen";
+  } else {
+    return "old loading screen";
+  }
+}
+```
+
+Types
+
+```typescript
+interface FetchFlagArgs<T> {
+  // An optional user ID to allow for user based targeting
+  userId?: string;
+  // This is a public key, and doesn't need to be kept secret
+  apiKey: string;
+  // The key of the project you're querying
+  projectKey: string;
+  // The key of the environment you're querying
+  environmentKey: string;
+  // The key of the flag you're querying
+  flagKey: string;
+  // An optional default value, will return undefined otherwise
+  defaultValue?: T;
+  // Whether or not to cache responses, defaults to true
+  useCache?: boolean;
+  // How long to cache, defaults to 60 seconds
+  cacheDuration?: {
+        interval: Interval;
+        duration: number;
+    };
+}
+export declare type Interval = "year" | "quarter" | "month" | "week" | "day" | "hour" | "minute" | "second";
+export declare function fetchFlag<T>({
+  userId,
+  apiKey,
+  projectKey,
+  environmentKey,
+  flagKey,
+  defaultValue,
+  useCache,
+  cacheDuration,
+}: FetchFlagArgs<T>): Promise<T | undefined>;
+```
--- lib/main.ts
+++ lib/main.ts
@@ -1,4 +1,4 @@
-interface FetchFlagArgs<T> {
+export interface FetchFlagArgs<T> {
   userId?: string;
   apiKey: string;
   projectKey: string;
@@ -6,6 +6,80 @@ interface FetchFlagArgs<T> {
   flagKey: string;
   defaultValue?: T;
   useCache?: boolean;
+  cacheDuration?: { interval: Interval; duration: number };
+}
+
+export type FetchFlagsArgs<T> = Omit<FetchFlagArgs<T>, "flagKey"> & {
+  flagKeys: string[];
+  resolveRemotely?: boolean;
+  defaultValue: { [key: string]: any };
+};
+
+type FetchFlagsRemotelyArgs = Omit<
+  Omit<FetchFlagsArgs<any>, "useCache">,
+  "cacheDuration"
+>;
+
+interface CachedValue {
+  expires_at: string;
+  value: any;
+}
+
+export type Interval =
+  | "year"
+  | "quarter"
+  | "month"
+  | "week"
+  | "day"
+  | "hour"
+  | "minute"
+  | "second";
+
+const zip = <X, Y>(a: X[], b: Y[]) => a.map((k, i) => [k, b[i]]);
+
+function dateAdd(
+  date: Date,
+  interval: Interval,
+  units: number
+): Date | undefined {
+  if (!(date instanceof Date)) return undefined;
+  var ret = new Date(date); //don't change original date
+  var checkRollover = function () {
+    if (ret.getDate() != date.getDate()) ret.setDate(0);
+  };
+  switch (String(interval).toLowerCase()) {
+    case "year":
+      ret.setFullYear(ret.getFullYear() + units);
+      checkRollover();
+      break;
+    case "quarter":
+      ret.setMonth(ret.getMonth() + 3 * units);
+      checkRollover();
+      break;
+    case "month":
+      ret.setMonth(ret.getMonth() + units);
+      checkRollover();
+      break;
+    case "week":
+      ret.setDate(ret.getDate() + 7 * units);
+      break;
+    case "day":
+      ret.setDate(ret.getDate() + units);
+      break;
+    case "hour":
+      ret.setTime(ret.getTime() + units * 3600000);
+      break;
+    case "minute":
+      ret.setTime(ret.getTime() + units * 60000);
+      break;
+    case "second":
+      ret.setTime(ret.getTime() + units * 1000);
+      break;
+    default:
+      (ret as any) = undefined;
+      break;
+  }
+  return ret;
 }
 
 export async function fetchFlag<T>({
@@ -15,19 +89,21 @@ export async function fetchFlag<T>({
   environmentKey,
   flagKey,
   defaultValue,
-  useCache
+  useCache,
+  cacheDuration,
 }: FetchFlagArgs<T>): Promise<T | undefined> {
   const isUsingCache = (useCache ?? true) && window.sessionStorage;
-
   const key = `projects/${projectKey}/environments/${environmentKey}/flags/${flagKey}`;
   if (isUsingCache) {
     const isCached = window.sessionStorage.getItem(`@firstkind/${key}`);
     if (isCached !== null) {
-        const payload = JSON.parse(isCached);
-        return payload?.[flagKey] ?? defaultValue;
+      const payload: CachedValue = JSON.parse(isCached);
+      if (new Date() < new Date(payload.expires_at)) {
+        return payload.value?.[flagKey] ?? defaultValue;
+      }
     }
   }
-  
+
   const resp = await fetch(`http://api.firstkindsoftware.com/v1/${key}`, {
     headers: {
       Accept: "application/json",
@@ -41,8 +117,98 @@ export async function fetchFlag<T>({
   const payload = await resp.json();
 
   if (isUsingCache) {
-    window.sessionStorage.setItem(`@firstkind/${key}`, JSON.stringify(payload));
+    const isDurationDefined =
+      cacheDuration?.interval !== undefined &&
+      cacheDuration?.duration !== undefined;
+    let tmp = { ...cacheDuration } as { interval: Interval; duration: number };
+    if (!isDurationDefined) {
+      tmp = {
+        interval: "second",
+        duration: 60,
+      };
+    }
+    const expires_at = dateAdd(
+      new Date(),
+      tmp.interval,
+      tmp.duration
+    )?.toISOString();
+
+    window.sessionStorage.setItem(
+      `@firstkind/${key}`,
+      JSON.stringify({
+        expires_at,
+        value: payload,
+      } as CachedValue)
+    );
   }
 
   return payload?.[flagKey] ?? defaultValue;
+}
+
+export async function fetchFlags<T>({
+  userId,
+  apiKey,
+  projectKey,
+  environmentKey,
+  flagKeys,
+  defaultValue,
+  useCache,
+  cacheDuration,
+  resolveRemotely,
+}: FetchFlagsArgs<T>): Promise<{ [key: string]: any } | undefined> {
+  if (resolveRemotely) {
+    return await fetchFlagsRemotely({
+      userId,
+      apiKey,
+      projectKey,
+      environmentKey,
+      flagKeys,
+      defaultValue,
+    });
+  }
+  const results = await Promise.all(
+    flagKeys.map((flagKey) =>
+      fetchFlag({
+        userId,
+        apiKey,
+        projectKey,
+        environmentKey,
+        flagKey,
+        defaultValue,
+        useCache,
+        cacheDuration,
+      })
+    )
+  );
+
+  return zip(flagKeys, results).reduce((acc, [k, v]) => {
+    acc[k] = v;
+    return acc;
+  }, {} as any);
+}
+
+async function fetchFlagsRemotely({
+  userId,
+  apiKey,
+  projectKey,
+  environmentKey,
+  flagKeys,
+  defaultValue,
+}: FetchFlagsRemotelyArgs): Promise<{ [key: string]: any } | undefined> {
+  const key = `projects/${projectKey}/environments/${environmentKey}/flags/${flagKeys.join(
+    ","
+  )}`;
+  const resp = await fetch(`http://api.firstkindsoftware.com/v1/${key}`, {
+    headers: {
+      Accept: "application/json",
+      "API-KEY": apiKey,
+      "USER-ID": userId ?? "",
+    },
+  });
+  if (resp.status === 404) {
+    return defaultValue;
+  }
+  const payload = await resp.json();
+
+  return payload ?? defaultValue;
 }
